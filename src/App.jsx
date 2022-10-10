05f3aef3a15fd72b604082a200a100158414c135
Florencio Britez <florenciobritez49@gmail.com>
2022-10-09 23:18:17 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div className="bg-darkBlue w-full h-[400px]">
+      <div className="bg-greyishBlue w-[100px] h-40">Trying whether the font works or dont</div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      red: "#FC4747",
+      darkBlue: "#10141E",
+      semiDarkBlue: "#1612DF",
+      greyishBlue: "#5A698F"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
